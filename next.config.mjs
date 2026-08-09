eb2b4984d9ae7521f2bb89170c2d0362897b0437
unknown <zmret13@gmail.com>
2026-08-09 15:05:37 +0700

    masih fixing supabase storage

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "dxlymbpdluttnqkgcwio.supabase.co",
+        hostname: "*.supabase.co",
         pathname: "/storage/v1/object/public/**",
       },
     ],
